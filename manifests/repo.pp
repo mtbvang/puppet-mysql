4bbb9a68d88c4940babbbca605c1d05b716e478a
Anthony Kuske <anthonykuske@gmail.com>
2014-03-10 20:02:41 +0800

    Make compatible with puppetlabs/apt

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -11,23 +11,41 @@ class mysql::repo
         'CD2EFD2A':
       }
 
-      apt::sources_list {"percona":
-          ensure  => present,
-          content => "deb http://repo.percona.com/apt $lsbdistcodename main",
-          require => Apt::Key['CD2EFD2A'],
+      # Puppetlabs/apt module
+      apt::source { 'percona':
+        ensure      => present,
+        include_src => true,
+        location    => 'http://repo.percona.com/apt',
+        release     => $::lsbdistcodename,
+        repos       => 'main',
+        notify      => Exec['mysql::repo::apt-get update'],
+        require     => Apt::Key['CD2EFD2A'],
       }
     }
 
-    'mariadb': { 
+    'mariadb': {
       apt::key {
         '1BB943DB':
       }
 
-      apt::sources_list {"mariadb5.3":
-          ensure  => present,
-          content => "deb http://mirror2.hs-esslingen.de/mariadb/repo/5.3/debian $lsbdistcodename main",
-          require => Apt::Key['1BB943DB'],
+      # Puppetlabs/apt module
+      # Sorry, ubuntu only
+      apt::source { 'mariadb5.5':
+        ensure      => present,
+        include_src => true,
+        location    => 'http://mirrors.coreix.net/mariadb/repo/5.5/ubuntu',
+        release     => $::lsbdistcodename,
+        repos       => 'main',
+        notify      => Exec['mysql::repo::apt-get update'],
+        require     => Apt::Key['CD2EFD2A'],
       }
     }
   }
+
+  exec { 'mysql::repo::apt-get update':
+    command     => 'apt-get update',
+    path        => '/usr/bin',
+    refreshonly => true,
+  }
+
 }
