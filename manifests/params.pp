1714d651f66aaf69dd9bf79014a6cb34e9cc5d79
Anthony Kuske <anthonykuske@gmail.com>
2014-03-10 20:58:24 +0800

    Up Oracle version to 5.5 and remove maatkit

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,15 @@
 class mysql::params
 {
   $packages = $mysql::type ? {
-    'oracle'  => [ 'mysql-server-5.1' ],
+    'oracle'  => [ 'mysql-server-5.5' ],
     'percona' => [ 'percona-server-server-5.6' ],
     'mariadb' => [ 'mariadb-server-5.5' ],
   }
 
   $packages_extra = $mysql::type ? {
-    'oracle'  => [ 'maatkit' ],
-    'percona' => [ 'maatkit', 'xtrabackup' ],
-    'mariadb' => [ 'maatkit'],
+    'oracle'  => [  ],
+    'percona' => [ 'xtrabackup' ],
+    'mariadb' => [ ],
   }
 
   $service = $mysql::type ? {
